e666ca40aa9aae77ffa0729ab2c771dc617d9f2a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/TimeSpanColumn/Default.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using DevExpress.Data.Filtering;
+
+namespace TimeSpanColumn {
+    public partial class _Default : System.Web.UI.Page {
+        protected void Page_Init(object sender, EventArgs e) {
+            ASPxGridView1.DataSource = GetDataTable();
+        }
+        protected void Page_Load(object sender, EventArgs e) {
+            ASPxGridView1.DataBind();
+            if(!IsPostBack && !IsCallback) {
+                ASPxGridView1.GroupBy(ASPxGridView1.Columns["TimeSpan"]);
+                CriteriaOperator filter = new OperandProperty("TimeSpan") == new OperandValue(new TimeSpan(1, 0, 0));
+                ASPxGridView1.FilterExpression = filter.ToString();
+            }
+        }
+
+        private DataTable GetDataTable() {
+            DataTable table = new DataTable();
+            table.Columns.Add("ID", typeof(int));
+            table.Columns.Add("TimeSpan", typeof(TimeSpan));
+            table.Rows.Add(1, new TimeSpan(1, 0, 0));
+            table.Rows.Add(2, new TimeSpan(0, 30, 0));
+            table.Rows.Add(3, new TimeSpan(3, 0, 0));
+            table.Rows.Add(4, new TimeSpan(0, 30, 0));
+            return table;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# The TimeSpan data support in the ASPxGridView
+
+
+<p>The ASPxGridView can display, sort and filter TimeSpan values. Here is a simple test project.</p>
+
+<br/>
+
+
