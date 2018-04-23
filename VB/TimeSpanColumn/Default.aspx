669f98f391d2740e84b7e623f1b9a94b5223267e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/TimeSpanColumn/Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="vb" AutoEventWireup="true" CodeBehind="Default.aspx.vb" Inherits="TimeSpanColumn._Default" %>
+
+<%@ Register Assembly="DevExpress.Web.ASPxGridView.v9.1, Version=9.1.5.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a"
+	Namespace="DevExpress.Web.ASPxGridView" TagPrefix="dxwgv" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+	<title>The TimeSpan data support in the ASPxGridView</title>
+</head>
+<body>
+	<form id="form1" runat="server">
+	<div>
+		<dxwgv:ASPxGridView ID="ASPxGridView1" runat="server">
+			<Settings ShowFilterBar="Visible" ShowGroupPanel="True" 
+				ShowHeaderFilterButton="True" />
+		</dxwgv:ASPxGridView>
+	</div>
+	</form>
+</body>
+</html>
---
+++ VB/TimeSpanColumn/Default.aspx.vb
@@ -0,0 +1,37 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Data
+Imports DevExpress.Data.Filtering
+
+Namespace TimeSpanColumn
+	Partial Public Class _Default
+		Inherits System.Web.UI.Page
+		Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
+			ASPxGridView1.DataSource = GetDataTable()
+		End Sub
+		Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+			ASPxGridView1.DataBind()
+			If (Not IsPostBack) AndAlso (Not IsCallback) Then
+				ASPxGridView1.GroupBy(ASPxGridView1.Columns("TimeSpan"))
+				Dim filter As CriteriaOperator = New OperandProperty("TimeSpan") = New OperandValue(New TimeSpan(1, 0, 0))
+				ASPxGridView1.FilterExpression = filter.ToString()
+			End If
+		End Sub
+
+		Private Function GetDataTable() As DataTable
+			Dim table As New DataTable()
+			table.Columns.Add("ID", GetType(Integer))
+			table.Columns.Add("TimeSpan", GetType(TimeSpan))
+			table.Rows.Add(1, New TimeSpan(1, 0, 0))
+			table.Rows.Add(2, New TimeSpan(0, 30, 0))
+			table.Rows.Add(3, New TimeSpan(3, 0, 0))
+			table.Rows.Add(4, New TimeSpan(0, 30, 0))
+			Return table
+		End Function
+	End Class
+End Namespace
